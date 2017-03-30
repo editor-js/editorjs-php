661aa912ea513e477b9d377e6be88605223bf758
George Berezhnoy <gohabereg@gmail.com>
2017-03-30 22:51:47 +0300

    List

--- CodexEditor/Tools/Lists.php
+++ CodexEditor/Tools/Lists.php
@@ -2,26 +2,48 @@
 
 namespace CodexEditor\Tools;
 
-use \CodexEditor\Factory;
+use CodexEditor\Factory;
 use \CodexEditor\Tools\Base;
 use \CodexEditor\Interfaces\HTMLPurifyable;
 use \HTMLPurifier;
 
-class Lists extends Base {
+class Lists extends Base implements HTMLPurifyable{
 
     public function initialize()
     {
-
+        $this->sanitize();
+        return $this->validate();
     }
 
     public function sanitize()
     {
-        // TODO: Implement sanitize() method.
+        $allowedTags = 'a[href],br,p,strong,b,i,em';
+
+        $sanitizer = clone $this->sanitizer;
+        $sanitizer->set('HTML.Allowed', $allowedTags);
+
+        $purifier = new HTMLPurifier($sanitizer);
+
+        foreach ($this->data['data']['items'] as $key => $item) {
+            $this->data['data']['items'][$key] = $purifier->purify($item);
+        }
     }
 
     public function validate()
     {
+        $validType      = is_array($this->data) && in_array($this->data['type'], Factory::getAllowedBlockTypes()['Paragraph']);
+        $itemsNotEmpty  = true;
 
+        foreach ($this->data['data']['items'] as $item) {
+            $itemsNotEmpty = $itemsNotEmpty && !empty($items);
+        }
+
+        if ($validType && $itemsNotEmpty) {
+            return true;
+        }
+
+        return null;
     }
+
 }
 
